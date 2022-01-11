291b2483e09279162edb1958ca5a9594ab7789f1
Koki-Nambu <ztailang414@gmail.com>
2022-01-11 15:00:37 +0900

    validates,ストロングパラメーターの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :nickname, presence: true
+  validates :first_name_full_width, presence: true
+  validates :last_name_full_width, presence:true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :date_of_birth, presence: true
+
 end
