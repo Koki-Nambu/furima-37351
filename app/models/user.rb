eede44d1d3cc395e30ae6c2e3f4f7303d203d29f
Koki-Nambu <ztailang414@gmail.com>
2022-01-11 15:34:17 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  validates :password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
   validates :nickname, presence: true
-  validates :first_name_full_width, presence: true
-  validates :last_name_full_width, presence:true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name_full_width, :last_name_full_width, presence:true, format:{with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name_kana, :last_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
   validates :date_of_birth, presence: true
 
 end
