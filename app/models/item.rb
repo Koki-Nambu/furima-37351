571e2b7aa210f71d9a57b24077e481eebcb861ef
Koki-Nambu <ztailang414@gmail.com>
2022-01-12 21:17:21 +0900

    ActiveHashの設定

---
+++ app/models/item.rb
@@ -0,0 +1,20 @@
+class Item < ApplicationRecord
+    belongs_to :user
+    has_one_attached :image
+  
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :days_to_ship
+    belongs_to :shipping_area
+    belongs_to :delivery_charge
+    belongs_to :status
+  
+    validates :title, :explanation, presence: true
+    validates :category_id, :days_to_ship_id, :shipping_area_id, :delivery_charge_id, :status_id, numericality: { other_than: 1 , message: "Can't be blank"} 
+    
+    with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+      validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                        presence: { message: "Can't be blank" }
+    end
+  
+end
