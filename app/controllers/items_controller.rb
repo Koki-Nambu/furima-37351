ab6b6af761845af759ffc79c069b89cb5115d23b
Koki-Nambu <ztailang414@gmail.com>
2022-01-13 18:07:05 +0900

    set_item定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :show]
-  before_action :set_item, only: [:show #:destroy]
+  before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :destroy]
 
 
   def index
@@ -36,7 +36,7 @@ class ItemsController < ApplicationController
   end
 
   def set_item
-    
+
     @item = Item.find(params[:id])
   end
 end
